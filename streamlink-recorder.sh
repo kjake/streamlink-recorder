5e861a8b50cf1ec114646338a97c7e9a75f74da0
KimPig <61234733+KimPig@users.noreply.github.com>
2023-08-12 02:38:47 +0900

    Update streamlink-recorder.sh

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -4,6 +4,6 @@
 
 while [ true ]; do
 	Date=$(date +%Y%m%d-%H%M%S)
-	streamlink $streamOptions $streamLink $streamQuality -o /home/download | ffmpeg -i pipe:0 -c copy -f mp4 /home/download/$streamName"-$Date".mp4
+	streamlink $streamOptions $streamLink $streamQuality -o /home/download | ffmpeg -i pipe:0 -c copy -f mp4 $streamName"-$Date".mp4
 	sleep 60s
 done
