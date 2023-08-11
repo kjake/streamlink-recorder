4e75193da44535462dd2f14835c776588b06d8de
KimPig <61234733+KimPig@users.noreply.github.com>
2023-08-12 03:50:02 +0900

    Change Filename and Format

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -3,7 +3,12 @@
 # For more information visit: https://github.com/downthecrop/TwitchVOD
 
 while [ true ]; do
-	Date=$(date +%Y%m%d-%H%M%S)
-	streamlink $streamOptions $streamLink $streamQuality -o /home/download | ffmpeg -i pipe:0 -c copy -f mp4 $streamName"-$Date".mp4
+    streamDate=$(date +"%Y-%m-%d %Hh%Mm%Ss")
+	# Get stream info as JSON
+	streamInfo=$(streamlink $streamOptions $streamLink $streamQuality -j)
+	# Extract stream title from JSON
+	streamTitle=$(echo $streamInfo | jq -r '.metadata.title')
+	# Download and convert stream
+	streamlink $streamOptions $streamLink $streamQuality -O | ffmpeg -i pipe:0 -c:v copy -c:a copy "/home/download/${streamName} - ${streamDate} - ${streamTitle}.mp4"
 	sleep 60s
 done
