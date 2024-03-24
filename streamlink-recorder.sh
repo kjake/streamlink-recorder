767db8d1eb8a847b96d3e1b6ae106a586c4ccc0a
kjake <kjake@users.noreply.github.com>
2024-03-24 14:17:59 -0400

    Update streamlink-recorder.sh
    - Support fragmented MP4 so that you can watch the stream still in-progress
    - Replace chars in a streamTitle which could cause issues with the file name

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -9,10 +9,10 @@ while [ true ]; do
 	# Assume that, if streamInfo is empty or actually returns an error message, the stream is not live
     if [ ! -z "$streamInfo" ] && ! echo "$streamInfo" | grep -q "error"; then
 		# Extract stream title from JSON
-		streamTitle=$(echo $streamInfo | jq -r '.metadata.title')
+		streamTitle=$(echo $streamInfo | jq -r '.metadata.title' | sed 's/[^a-zA-Z0-9]/_/g')
 
 		# Download and convert stream
-		streamlink $streamOptions $streamLink $streamQuality --stdout | ffmpeg -fflags +discardcorrupt -i pipe:0 -c copy -bsf:a aac_adtstoasc -f mp4 -loglevel error -y "/home/download/${streamName} - ${streamDate} - ${streamTitle}.mp4"
+		streamlink $streamOptions $streamLink $streamQuality --stdout | ffmpeg -fflags +discardcorrupt -i pipe:0 -c copy -bsf:a aac_adtstoasc -movflags +frag_keyframe+empty_moov+default_base_moof -f mp4 -loglevel error -y "/home/download/${streamName} - ${streamDate} - ${streamTitle}.mp4"
 	else
 		echo $streamInfo | jq -r '.error'
 	fi
