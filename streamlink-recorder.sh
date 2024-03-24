cde7be046c3955bdf377a50edcd853993a1e433c
kjake <kjake@users.noreply.github.com>
2024-03-24 13:32:48 -0400

    Update streamlink-recorder.sh
    - Make ffmpeg less verbose
    - Assume that, if streamInfo is empty, the stream is not live

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -6,9 +6,14 @@ while [ true ]; do
     streamDate=$(date +"%Y-%m-%d %Hh%Mm%Ss")
 	# Get stream info as JSON
 	streamInfo=$(streamlink $streamOptions $streamLink $streamQuality -j)
-	# Extract stream title from JSON
-	streamTitle=$(echo $streamInfo | jq -r '.metadata.title')
-	# Download and convert stream
-	streamlink $streamOptions $streamLink $streamQuality --stdout | ffmpeg -fflags +discardcorrupt -i pipe:0 -c copy -bsf:a aac_adtstoasc -f mp4 -y "/home/download/${streamName} - ${streamDate} - ${streamTitle}.mp4"
+		# Assume that, if streamInfo is empty, the stream is not live
+	    if [ ! -z "$streamInfo" ]; then
+
+		# Extract stream title from JSON
+		streamTitle=$(echo $streamInfo | jq -r '.metadata.title')
+
+		# Download and convert stream
+		streamlink $streamOptions $streamLink $streamQuality --stdout | ffmpeg -fflags +discardcorrupt -i pipe:0 -c copy -bsf:a aac_adtstoasc -f mp4 -loglevel error -y "/home/download/${streamName} - ${streamDate} - ${streamTitle}.mp4"
+	fi
 	sleep ${streamPoll:-60}s
 done
