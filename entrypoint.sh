e9a5d69ccd862ddbd058f48f50d8c37e1f5d15e2
kjake <kjake@users.noreply.github.com>
2024-03-24 15:00:46 -0400

    Update entrypoint.sh
    [skip ci]

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@
 USER_ID=${uid:-9001}
 GROUP_ID=${gid:-9001}
 
-echo -e "UID : $USER_ID \nGID : $GROUP_ID"
+echo -e "UID : $USER_ID\nGID : $GROUP_ID\n"
 
 chown -R $USER_ID:$GROUP_ID /home/plugins
 chown -R $USER_ID:$GROUP_ID /home/script
