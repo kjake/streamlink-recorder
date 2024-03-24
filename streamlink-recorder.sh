b01787e1fbdf0dbbb1b1ab5389826dcd6fed8599
kjake <kjake@users.noreply.github.com>
2024-03-24 14:42:56 -0400

    Update streamlink-recorder.sh
    Print the filename out

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -10,8 +10,8 @@ while [ true ]; do
     if [ ! -z "$streamInfo" ] && ! echo "$streamInfo" | grep -q "error"; then
 		# Extract stream title from JSON
 		streamTitle=$(echo $streamInfo | jq -r '.metadata.title' | sed 's/[^a-zA-Z0-9]/_/g')
-
 		# Download and convert stream
+		echo "Saving to: ${streamName} - ${streamDate} - ${streamTitle}.mp4"
 		streamlink $streamOptions $streamLink $streamQuality --stdout | ffmpeg -fflags +discardcorrupt -i pipe:0 -c copy -bsf:a aac_adtstoasc -movflags +frag_keyframe+empty_moov+default_base_moof -f mp4 -loglevel error -y "/home/download/${streamName} - ${streamDate} - ${streamTitle}.mp4"
 	else
 		echo $streamInfo | jq -r '.error'
